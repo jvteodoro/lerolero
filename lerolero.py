68c875c756c31431591d263e15dbfba936a8e306
Ada Lovelace <diegofavila20@gmail.com>
2025-03-20 10:45:45 -0300

    Words and new language

--- lerolero.py
+++ lerolero.py
@@ -9,9 +9,16 @@ import random
 # listas de possiblidades para cada uma das partes.
 #
 
-parte1 = []
-parte2 = []
-parte3 = []
+parte1 = ["oi", "como"]
+parte2 = ["maçã", "pera"]
+parte3 = ["cavalo", "zebra"]
+
+lingua = int(input("Escolha a língua: 1 - português; 2 - inglês\n"))
+
+if lingua == 2:
+	parte1 = ["hi", "como"]
+	parte2 = ["apple", "pear"]
+	parte3 = ["horse", "zebra"]
 
 # Combina as partes aleatoriamentes
 #
