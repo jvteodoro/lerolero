32c131408e97e649f6dbed612d5b343a9c390c46
Alan Turning <joao.v.teodoro@usp.br>
2025-03-20 10:33:47 -0300

    Esqueleto do programa

--- lerolero.py
+++ lerolero.py
@@ -3,3 +3,16 @@
 Gerador de LEROLERO
 Gera frases de efeito sem significado real.
 """
+import random
+
+# Cada frase é composa por três partes aleatórios; aqui,
+# listas de possiblidades para cada uma das partes.
+#
+
+parte1 = []
+parte2 = []
+parte3 = []
+
+# Combina as partes aleatoriamentes
+#
+print (random.choice(parte1), random.choice(parte2), random.choice(parte3))
